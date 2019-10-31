8ff06fa8d1cc9c4d1116f573dc93500fff5b8942
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-10-31 07:06:50 -0300

    Animate press start

--- scripts/StartUI.gd
+++ scripts/StartUI.gd
@@ -1,8 +1,16 @@
 extends Control
 
 var next_scene = preload("res://scenes/Game.tscn")
+var show_time = 0
+var show_time_delay = 0.2
+var is_show = true
 
 func _process(delta):
+	show_time += delta
+	if show_time >= show_time_delay:
+		show_time = 0
+		is_show = !is_show
+		$CenterContainer/Container/lbPressStart.visible = is_show
 	if Input.is_action_pressed("ui_jump"):
 		get_tree().change_scene_to(next_scene)
 
