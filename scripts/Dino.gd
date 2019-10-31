b536911e51acebd7c7b27815a42ea7d2fef1126e
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-10-30 22:27:15 -0300

    Create parallax background and script of the jump for Dino

---
+++ scripts/Dino.gd
@@ -0,0 +1,36 @@
+extends Area2D#
+
+const FLOOR = Vector2(240, 930)
+const GRAVITY = 3000
+const GRAVITY_MODIFIER = 2
+const JUMP_HEIGHT = -1200
+
+var velocity = Vector2()
+var is_running = false
+
+func _ready():
+	set_position(FLOOR)
+	pass
+
+func _physics_process(delta):	
+	if Input.is_action_pressed("ui_jump"):
+		velocity.y += GRAVITY * delta
+	else:
+		velocity.y +=  GRAVITY * delta * GRAVITY_MODIFIER
+		
+	if Input.is_action_just_pressed("ui_jump") and position == FLOOR:
+		$Sprite.play("jump")
+		velocity.y = JUMP_HEIGHT
+		
+	position += velocity * delta
+		
+	if get_position().y > FLOOR.y:
+		if is_running:
+			$Sprite.play("run")
+		else:
+			$Sprite.play("walk")
+		set_position(FLOOR)
+		velocity = Vector2()
+		
+	
+
---
+++ scripts/ParallaxBackground.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+var parallax_offset = 0
+
+func _ready():
+	pass
+	
+func _process(delta):
+	parallax_offset -= delta * 100
+	set_scroll_offset(Vector2(parallax_offset, 0))
+	pass
+
