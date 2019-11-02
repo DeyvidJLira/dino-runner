8055c74085ce413cf26ea6dd017122fe9d204e73
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-11-02 09:43:48 -0300

    Implement lifetime for alien

--- scripts/Alien.gd
+++ scripts/Alien.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 onready var player = get_parent().get_node("Dino")
 
+var life_time = 7
 var is_dead = false
 var ground = Vector2(2100, 928)
 var speed_factor = 3
@@ -10,6 +11,9 @@ func _ready():
 	set_position(ground)
 	
 func _physics_process(delta):
+	life_time -= delta
+	if life_time <= 0:
+		queue_free()
 	if GameVariables.game_state == 1 and !is_dead:
 		set_position(position + GameVariables.speed * speed_factor * Vector2.LEFT * delta)
 
